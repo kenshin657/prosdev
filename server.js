48645a6ac9bca34b9f0f471049882d601f3cf4d2
feebeef <phoebe_chen@dlsu.edu.ph>
2020-07-13 18:07:57 +0800

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ mongoose.Promise = global.Promise
 const MONGOLAB_URI = "mongodb+srv://user1:okay@cluster0.diqll.mongodb.net/sample_airbnb?retryWrites=true&w=majority";
 
 mongoose.connect(MONGOLAB_URI, {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   }).catch(err => console.log(err))
 
 app.use(urlencoder);
